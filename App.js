1cf361e8e8df699af92edefe38385ac55d767f32
rontend <donggyu9410@gmail.com>
2019-03-29 19:19:26 +0900

    Change splash.png image, add new font(open sans), add AppLoading componend

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AppLoading, Asset, Font } from "expo";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false
+    };
+  }
   render() {
+    const { _appLoaded, _onError, _cacheResourcesAsync } = this;
+    if (!this.state.loaded) {
+      return (
+        <AppLoading
+          startAsync={_cacheResourcesAsync}
+          onFinish={_appLoaded}
+          onError={_onError}
+        />
+      );
+    }
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.text}>
+          Open up App.js to start working on your app!
+        </Text>
       </View>
     );
   }
+
+  _cacheResourcesAsync = async () => {
+    await Font.loadAsync({
+      "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+      "open-sans-light": require("./assets/fonts/OpenSans-Light.ttf"),
+      "open-sans-regular": require("./assets/fonts/OpenSans-Regular.ttf")
+    });
+  };
+
+  _appLoaded = () => {
+    this.setState({
+      loaded: true
+    });
+  };
+
+  _onError = err => {
+    console.log(err);
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  text: {
+    fontFamily: "open-sans-bold"
+  }
 });
