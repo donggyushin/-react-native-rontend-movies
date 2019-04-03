eb53dc4c87177e9731dddcbac4b20d601ebf6855
rontend <donggyu9410@gmail.com>
2019-04-03 15:22:08 +0900

    Changed styles of movie screen from stylesheet to styled-component.

--- screens/movies/moviesPresenter.js
+++ screens/movies/moviesPresenter.js
@@ -1,33 +1,35 @@
 import React from "react";
-import { View, Text, StatusBar, Button, ActivityIndicator } from "react-native";
+import { View, Text, StatusBar, Button } from "react-native";
 import { COLORS } from "../../contants/colors";
 import { SIZES } from "../../contants/sizes";
 import LoadingComponent from "../../components/loadingComponent";
+import styled from "styled-components";
+
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: ${COLORS.bgColor};
+  color: ${COLORS.fontColor};
+`;
+
+const TempText = styled.Text`
+  font-size: ${SIZES.big};
+  color: ${COLORS.fontColor};
+`;
+
+const GoToDetailButton = styled.Button``;
 
 const MoviesPresenter = ({ _goToDetails, loading }) => {
   if (loading) {
     return <LoadingComponent />;
   }
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: COLORS.bgColor
-      }}
-    >
+    <Container>
       <StatusBar barStyle={"light-content"} />
-      <Text
-        style={{
-          color: COLORS.fontColor,
-          fontSize: SIZES.big
-        }}
-      >
-        Movies
-      </Text>
-      <Button onPress={_goToDetails} title={"Go To Details"} />
-    </View>
+      <TempText>Movies</TempText>
+      <GoToDetailButton onPress={_goToDetails} title={"Go To Details"} />
+    </Container>
   );
 };
 
