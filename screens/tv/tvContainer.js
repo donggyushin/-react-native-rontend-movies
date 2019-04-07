9ede0eac02414bcccf68cfc3059ad198e56ae6d4
rontend <donggyu9410@gmail.com>
2019-04-07 21:03:41 +0900

    Get tv apis from movie db api

--- screens/tv/tvContainer.js
+++ screens/tv/tvContainer.js
@@ -1,8 +1,49 @@
 import React from "react";
 import TvPresenter from "./tvPresenter";
+import { TV_API } from "../../api";
 
 export default class TvContainer extends React.Component {
+  state = {
+    loading: true,
+    popular: null,
+    onTheAir: null,
+    recommendations: null,
+    error: null
+  };
+
+  async componentDidMount() {
+    let error, popular, onTheAir, recommendations;
+    try {
+      popular = await TV_API.getPopular();
+      popular = popular.data.results;
+      onTheAir = await TV_API.onTheAir();
+      onTheAir = onTheAir.data.results;
+      recommendations = await TV_API.recommendations();
+      recommendations = recommendations.data.results;
+    } catch (error) {
+      error = error;
+    } finally {
+      this.setState({
+        loading: false,
+        popular,
+        onTheAir,
+        recommendations,
+        error
+      });
+    }
+  }
+
   render() {
-    return <TvPresenter />;
+    const { loading, error, popular, onTheAir, recommendations } = this.state;
+
+    return (
+      <TvPresenter
+        loading={loading}
+        error={error}
+        popular={popular}
+        onTheAir={onTheAir}
+        recommendations={recommendations}
+      />
+    );
   }
 }
--- screens/tv/tvPresenter.js
+++ screens/tv/tvPresenter.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Text, View } from "react-native";
 import { COLORS } from "../../contants/colors";
 import { SIZES } from "../../contants/sizes";
 import styled from "styled-components";
+import LoadingComponent from "../../components/loadingComponent";
 
 const Container = styled.View`
   flex: 1;
@@ -16,7 +16,11 @@ const Title = styled.Text`
   color: ${COLORS.fontColor};
 `;
 
-const Tv = () => {
+const Tv = ({ loading, error, popular, onTheAir, recommendations }) => {
+  if (loading) {
+    return <LoadingComponent />;
+  }
+  console.log(popular);
   return (
     <Container>
       <Title>TV</Title>
